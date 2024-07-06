b855b6495290bf1356b708a7325cf8ac52da9648
ronakadroja <ronakadroja1234@gmail.com>
2024-07-06 17:44:14 +0530

    popularNovel

--- app/(pages)/HomePage/PopularNovels.js
+++ app/(pages)/HomePage/PopularNovels.js
@@ -1,76 +1,58 @@
 'use client'
-import useApiService from '@/services/ApiService';
-import Image from 'next/image';
+import Image from 'next/image'
+import { useRouter } from 'next/navigation';
 import Link from 'next/link';
-import { useEffect, useState } from 'react';
-import 'swiper/css';
-import 'swiper/css/free-mode';
-import { FreeMode, Mousewheel } from 'swiper/modules';
+import { useState, useEffect } from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/free-mode'
+import { FreeMode, Mousewheel } from 'swiper/modules'
+import useApiService from '@/services/ApiService';
 
-function PopularNovels() {
+function PopularNovels(props) {
+    const router = useRouter();
+    const { getMostPopularNovels } = useApiService();
     const [mostPopularNovelData, setMostPopularNovelData] = useState([]);
+    const [title, setTitleIndex] = useState(null);
 
     useEffect(() => {
-        const fetchMostPopularNovels = async () => {
-            try {
-                const res = await useApiService().getMostPopularNovels();
+        getMostPopularNovels()
+            .then((res) => {
                 setMostPopularNovelData(res?.data?.data?.data || []);
-            } catch (err) {
-                console.error('Error fetching most popular novels:', err);
-            }
-        };
-
-        fetchMostPopularNovels();
+            })
+            .catch((err) => {
+                console.error('Error fetching popular novels:', err);
+            });
     }, []);
 
     return (
         <div className='md:pt-10 pt-10 px-4 md:px-8'>
             <div className='flex justify-between items-center pb-5'>
                 <div className='text-2xl md:text-2xl font-bold'>Most Popular Novels</div>
-                <Link href='/novel-list/popular-More' prefetch className='underline cursor-pointer'>See More</Link>
+                <Link href={{ pathname: `/novel-list/popular-More` }} prefetch className='underline cursor-pointer'>See More</Link>
             </div>
             <div className='md:gap-x-4 flex md:hidden'>
                 <Swiper
                     slidesPerView={2}
                     spaceBetween={10}
                     freeMode={true}
-                    pagination={false}
-                    modules={[FreeMode]}
                     breakpoints={{
                         640: { slidesPerView: 3 },
                         768: { slidesPerView: 4 },
                         1024: { slidesPerView: 6 },
-                    }}
-                >
+                    }}>
                     {mostPopularNovelData.map((item, index) => (
                         <SwiperSlide key={index}>
-                            <div className='NewReleaseCard cursor-pointer rounded-2xl overflow-hidden'>
+                            <div className={`NewReleaseCard cursor-pointer rounded-2xl overflow-hidden`}>
                                 <Link href={`/detail/view/${item?._id}`} prefetch>
-                                    <Image
-                                        src={item?.coverImg || ''}
-                                        height={300}
-                                        width={300}
-                                        alt='cover'
-                                        className='releaseImage'
-                                    />
+                                    <Image src={item?.coverImg || ""} height={300} width={300} alt='cover' className='releaseImage' />
                                 </Link>
                                 <div className='info'>
                                     <h1 className='font-semibold'>{item?.title}</h1>
                                     <p>{item?.description?.slice(0, 200)}</p>
                                 </div>
-                                <div
-                                    onClick={() => setTitleIndex(index)}
-                                    className='hidden md:block text-white font-semibold gradientClassCards text-center text-sm py-1 absolute bottom-0 w-full rounded-b-xl z-10'
-                                >
-                                    {item?.title}
-                                </div>
-                                <div
-                                    onClick={() => setTitleIndex(index)}
-                                    className='block md:hidden text-white font-semibold gradientClassCards text-center text-sm py-1 absolute bottom-0 w-full rounded-b-xl z-10'
-                                >
-                                    {item?.title?.length > 15 ? `${item?.title?.slice(0, 15)}..` : item?.title}
-                                </div>
+                                <div onClick={() => setTitleIndex(index)} className="hidden md:block text-white font-semibold gradientClassCards text-center text-sm py-1 absolute bottom-0 w-full rounded-b-xl z-10">{item?.title}</div>
+                                <div onClick={() => setTitleIndex(index)} className="block md:hidden text-white font-semibold gradientClassCards text-center text-sm py-1 absolute bottom-0 w-full rounded-b-xl z-10">{item?.title?.slice(0, 15)}</div>
                             </div>
                         </SwiperSlide>
                     ))}
@@ -83,25 +65,16 @@ function PopularNovels() {
                     spaceBetween={8}
                     freeMode={true}
                     mousewheel={true}
-                    pagination={false}
-                    modules={[Mousewheel, FreeMode]}
                     breakpoints={{
                         640: { slidesPerView: 3 },
                         768: { slidesPerView: 4 },
                         1024: { slidesPerView: 6 },
-                    }}
-                >
+                    }}>
                     {mostPopularNovelData.map((item, index) => (
-                        <SwiperSlide key={index} className='containerImage cursor-pointer'>
+                        <SwiperSlide key={index} className="containerImage cursor-pointer">
                             <Link href={`/detail/view/${item?._id}`} prefetch>
-                                <Image
-                                    src={item?.coverImg || ''}
-                                    height={300}
-                                    width={300}
-                                    alt='cover'
-                                    className='rounded-md h-[280px] object-cover'
-                                />
-                                <div className='textImage'>
+                                <Image src={item?.coverImg || ""} height={300} width={300} alt='cover' className='rounded-md h-[280px] object-cover' />
+                                <div className="textImage">
                                     <h1 className='font-semibold'>{item?.title?.slice(0, 30)}</h1>
                                     <p>{item?.description?.slice(0, 70)}</p>
                                 </div>
